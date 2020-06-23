3db5180033045b2a1aca3f4b43d2b40aa17b70a1
anhtuan-it <66792742+anhtuan-it@users.noreply.github.com>
2020-06-23 20:51:54 +0700

    Update home.blade.php
    Set height image

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -9,7 +9,7 @@
         @foreach ($data as $item)
         <div class="col-lg-3 col-md-6 mb-4">
             <div class="card h-100">
-            <a href="{{URL::to('/../wallpapers/public/upload/image/'.$item->img_name)}}"><img class="card-img-top" src="/../wallpapers/public/upload/image/{{$item->img_name}}" alt="" width="300px" height="300px"></a>
+            <a href="{{URL::to('/../wallpapers/public/upload/image/'.$item->img_name)}}"><img class="card-img-top" src="/../wallpapers/public/upload/image/{{$item->img_name}}" alt="" width="300px" height="400px"></a>
                 <div class="card-body">
                     {{$item->img_type}}
                 </div>
